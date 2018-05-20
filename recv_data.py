3ba7d0762406ed817cab33e84439a8e7898f5661
Skryabin Gleb <glebka.sk@gmail.com>
2018-05-20 20:11:54 +0300

    Fixes in recv and send data scripts

--- recv_data.py
+++ recv_data.py
@@ -10,19 +10,18 @@ host = socket.gethostname() # Get local machine name
 port = 8184                 # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
 s.listen(5)                 # Now wait for client connection.
-while True:
-    for file in workFiles:
+c, addr = s.accept()     # Establish connection with client.
+for file in workFiles:
+    while True:
         f = open(file, 'wb')
-        c, addr = s.accept()     # Establish connection with client.
         print 'Got connection from', addr
         print "Receiving..."
         l = c.recv(1024)  # this receives 13 bytes which is corrupting the data
         l = c.recv(1024)
         while (l):
-            print "Receiving..."
             f.write(l)
             l = c.recv(1024)
         f.close()
-    print "Done Receiving"
-    c.send('Thank you for connecting')
-    c.close()                # Close the connection
+print "Done Receiving"
+c.send('Thank you for connecting')
+c.close()                # Close the connection
--- send_data.py
+++ send_data.py
@@ -31,7 +31,7 @@ for file in workFiles:
         s.send(l)
         l = f.read(1024)
     f.close()
-    print "Done Sending"
     print s.recv(1024)
+print "Done Sending"
 s.shutdown(socket.SHUT_WR)
 s.close()                     # Close the socket when done
