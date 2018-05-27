c79da7f6210b3208ea37ec953aa919473a3ea4bf
Gleb Nurov <glnur@mail.ru>
2018-05-27 05:39:17 +0300

    Now Recv_data is a demon

--- recv_data.py
+++ recv_data.py
@@ -9,19 +9,20 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         # Create a socket 
 host = socket.gethostname() # Get local machine name
 port = 8184                 # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
-s.listen(5)                 # Now wait for client connection.
-c, addr = s.accept()     # Establish connection with client.
-for file in workFiles:
-    while True:
-        f = open(file, 'wb')
-        print 'Got connection from', addr
-        print "Receiving..."
-        l = c.recv(1024)  # this receives 13 bytes which is corrupting the data
-        l = c.recv(1024)
-        while (l):
-            f.write(l)
+while True:
+    s.listen(5)                 # Now wait for client connection.
+    c, addr = s.accept()     # Establish connection with client.
+    for file in workFiles:
+        while True:
+            f = open(file, 'wb')
+            print 'Got connection from', addr
+            print "Receiving..."
+            l = c.recv(1024)  # this receives 13 bytes which is corrupting the data
             l = c.recv(1024)
-        f.close()
-print "Done Receiving"
-c.send('Thank you for connecting')
-c.close()                # Close the connection
+            while (l):
+                f.write(l)
+                l = c.recv(1024)
+            f.close()
+    print "Done Receiving"
+    c.send('Thank you for connecting')
+    c.close()                # Close the connection
